465102d1f23c7cfe3ed7d2fe61130a71ba9c9bce
a23 <takahiroatsumi0517@gmail.com>
2023-08-14 17:07:17 +0900

    Prevent cron on migration

--- chat/apps.py
+++ chat/apps.py
@@ -6,6 +6,9 @@ class ChatConfig(AppConfig):
     name = 'chat'
 
     def ready(self, ):
+        import sys
+        if ('migrate' in sys.argv or 'makemigration' in sys.argv):
+            return
         from django_apscheduler.jobstores import register_events
         from chat.crons import scheduler
         register_events(scheduler)
